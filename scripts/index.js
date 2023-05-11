a2d9072ee64492de8509058ddd1e78fe4cce6d48
Chauni <chaunidbrown@gmail.com>
2023-05-11 13:14:19 -0400

    got the values to print from the array

--- scripts/index.js
+++ scripts/index.js
@@ -18,4 +18,23 @@ let menu = {
     ]
 };
 
+const memberDisplayEl = document.getElementById('memberDisplay');
 const categorySelectorEl = document.getElementById('categorySelector');
+categorySelectorEl.onchange = onCategorySelectorChange;
+
+function onCategorySelectorChange() {
+    const selectedValue = categorySelectorEl.value
+
+    console.log(selectedValue)
+
+    if (selectedValue == 'drinks') {
+        console.log(menu.drinks)
+    } if (selectedValue == 'entrees') {
+        console.log(menu.entrees)
+    } if (selectedValue == 'desserts') {
+        console.log(menu.desserts)
+    } else if (selectedValue == "") {
+        alert("Select a category")
+    }
+
+}
